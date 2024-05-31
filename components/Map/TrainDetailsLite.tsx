512dff954bb1f60b33f2b578c59ee49b5e627164
Rekinowy <107859450+Rekinowy@users.noreply.github.com>
2024-05-31 12:55:45 +0200

    change details lite max width

--- components/Map/TrainDetailsLite.tsx
+++ components/Map/TrainDetailsLite.tsx
@@ -35,7 +35,7 @@ const TrainDetailsLite = ({
     <div
       className={`${
         !showDetailsLite && "md:hidden"
-      } flex flex-col absolute px-2 py-2 w-[80%] max-w-[280px] md:max-w-[260px] bottom-8 right-1/2 md:right-3 z-[1000] max-md:transform max-md:translate-x-1/2 rounded-xl border-1 border-slate-400 dark:border-slate-800 text-primary_dark dark:text-white bg-light_primary/90 dark:bg-primary/90  backdrop-blur-sm ${
+      } flex flex-col absolute px-2 py-2 w-[80%] max-w-[280px] bottom-8 right-1/2 md:right-3 z-[1000] max-md:transform max-md:translate-x-1/2 rounded-xl border-1 border-slate-400 dark:border-slate-800 text-primary_dark dark:text-white bg-light_primary/90 dark:bg-primary/90  backdrop-blur-sm ${
         view === "timetable" ? "max-h-[60dvh]" : "max-h-[30dvh]"
       } transition-all`}
     >
