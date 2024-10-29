b24855072f6cee13699f5cf14201ec2b69f6bb1b
Rekinowy <107859450+Rekinowy@users.noreply.github.com>
2024-10-29 18:51:26 +0100

    adjust header in mobile view

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -20,7 +20,7 @@ const Home = async ({ params: { locale } }: { params: { locale: string } }) => {
       />
       <main className="flex flex-col h-[100dvh] items-center p-4 lg:p-8">
         <Header />
-        <section className="p-8 text-4xl sm:text-5xl text-slate-200 font-roboto drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+        <section className="pt-8 pb-4 md:pb-8 text-3xl sm:text-4xl md:text-5xl text-slate-200 font-roboto drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
           <h2 style={{ textShadow: "1px 1px 1px #1d2935" }}>{t("header")}</h2>
         </section>
         <ServersList />
--- components/Home/Header.tsx
+++ components/Home/Header.tsx
@@ -15,16 +15,17 @@ const Header = () => {
   return (
     <header className="flex w-full z-10 gap-2 items-center justify-between">
       <div className="flex gap-3 items-center">
-        <div className="w-16 h-16 md:w-[72px] md:h-[72px]">
+        <div className="w-14 h-14 md:w-[72px] md:h-[72px]">
           <Image width={72} height={72} src={"/favicon.png"} alt="logo" className="shadow-lg" />
         </div>
-        <h1 className="md:pt-1 font-medium leading-3" style={{ textShadow: "2px 2px 2px #1d2935" }}>
-          <span className="text-3xl md:text-4xl tracking-wider text-light_primary_light">SimRail</span>
-          <br />
-          <span className="text-xl md:text-2xl tracking-[0.2em] text-light_gray">Live Map</span>
-        </h1>
+        <div className="flex flex-col font-medium" style={{ textShadow: "2px 2px 2px #1d2935" }}>
+          <h1 className="text-2xl md:text-4xl tracking-wider leading-6 text-light_primary_light">SimRail</h1>
+          <h2 className="text-lg md:text-2xl tracking-[0.14em] md:tracking-[0.2em] leading-6 text-light_gray">
+            Live Map
+          </h2>
+        </div>
       </div>
-      <div className="flex gap-4">
+      <div className="flex gap-3 md:gap-4">
         <button
           onClick={toggleTheme}
           className={`flex p-4 items-center bg-light_primary/90 dark:bg-primary/80 hover:bg-light_primary_light dark:hover:bg-primary text-primary dark:text-slate-200 hover:text-primary_dark dark:hover:text-white text-lg border rounded-lg border-slate-400 hover:border-slate-600 dark:border-slate-800 dark:hover:border-slate-900 hover:scale-[1.01] transition duration-100`}
