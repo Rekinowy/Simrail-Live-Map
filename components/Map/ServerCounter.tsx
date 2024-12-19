ccf73f0ceb6cd027f09454920623cdb8e9f259e8
Rekinowy <107859450+Rekinowy@users.noreply.github.com>
2024-12-19 02:43:04 +0100

    add backdrop blur to server counter

--- components/Map/ServerCounter.tsx
+++ components/Map/ServerCounter.tsx
@@ -65,6 +65,7 @@ const ServerCounter = ({
 
   return (
     <div className="absolute bottom-5 md:bottom-4 left-3 z-[1200] min-w-[126px] flex flex-col gap-0.5 md:gap-1 items-center px-2 py-1 text-slate-700/90 dark:text-light_gray/90 bg-light_primary/80 dark:bg-primary/80 shadow-xl border-1 border-slate-400 dark:border-slate-800 rounded-lg ">
+      <div className="absolute w-full h-full top-0 left-0 backdrop-blur-md rounded-lg -z-10" />
       <div className="flex items-center justify-between gap-3 w-full h-8">
         <div className="flex w-1/2 items-center justify-between h-full">
           <div className="font-medium text-center text-lg leading-[1.3rem]">{serverCode.toUpperCase()}</div>
